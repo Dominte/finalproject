2af4d456abc7367d16c17d8cf11bc13070b8a2e7
Dominte Florin <dominte.florin.iulian@gmail.com>
2021-05-09 02:39:22 +0300

    Model setup

--- src/main/java/com/example/finalproject/models/User.java
+++ src/main/java/com/example/finalproject/models/User.java
@@ -1,6 +1,7 @@
 package com.example.finalproject.models;
 
 
+import com.example.finalproject.utils.RoleEnum;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,12 +23,17 @@ public class User {
     private Long id;
 
     @NotNull
+    @Column(name = "first_name")
     private String firstName;
 
     @NotNull
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email", unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    private RoleEnum role;
+
 
 }
---
+++ src/main/java/com/example/finalproject/utils/RoleEnum.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.utils;
+
+public enum RoleEnum {
+    Student,
+    Teacher,
+    Admin
+}
